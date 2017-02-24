3f2118210e1ae6bc87c197c6b5c82957bb057724
Hotbread <j_painchaud@orange.fr>
2017-02-24 16:37:00 +0100

    premier commit template fini

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Author.create(name: "Robert de Niro", birthdate: "1953")
+Author.create(name: "Julia roberts", birthdate: "1933")
+Author.create(name: "Al PacinO", birthdate: "1922")
+Author.create(name: "james bond", birthdate: "1973")
+Author.create(name: "Superman", birthdate: "1967")
